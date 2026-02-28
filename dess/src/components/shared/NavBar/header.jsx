4d1e887642b8629944d8e469fc4b197003fdb169
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-02-28 13:26:50 -0300

    feat: add global styles, layout, and header component
    - Created globals.css for styling with Tailwind CSS variables.
    - Implemented layout.jsx to set up the root layout with font imports.
    - Added page.jsx as the main entry point for the application.
    - Developed header.jsx for the navigation bar structure.

---
+++ dess/src/components/shared/NavBar/header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <main className="w-full">
+            <header className="max-w-300 mx-auto">
+                <div>LOGO</div>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div>
+                    <button></button>
+                </div>
+            </header>
+        </main>
+    )
+}
