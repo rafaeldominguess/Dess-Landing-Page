6d31e36dc13948dcfec8c86c9864f0f75c4673e2
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-03-06 19:32:32 -0300

    fix: update Header component title and styling

--- dess/src/components/shared/NavBar/header.jsx
+++ dess/src/components/shared/NavBar/header.jsx
@@ -6,8 +6,8 @@ function Header() {
             <header className="max-w-350 mx-auto flex justify-between items-center h-4 px-6">
 
                 
-                    <div className="text-(--color) text-[32px] font-extrabold ">
-                        Dess
+                    <div className="text-(--color) text-xl font-bold">
+                        Ana Fernandes
                     </div>
 
                     <div className="flex gap-16 text-(--color) text-md font-bold cursor-pointer">
