bfacdacf00e528abf70467b8d82bddc40a59167e
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-04-13 19:47:00 -0300

    fix: format code for consistent indentation in sitemap.js

--- dess/src/app/robots.js
+++ dess/src/app/robots.js
@@ -1,13 +1,13 @@
 export default function robots() {
-  const siteUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || "https://www.anafernandesstudio.com.br";
+    const siteUrl =
+        process.env.NEXT_PUBLIC_SITE_URL || "https://www.anafernandesstudio.com.br";
 
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
-  };
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: `${siteUrl}/sitemap.xml`,
+        host: siteUrl,
+    };
 }
